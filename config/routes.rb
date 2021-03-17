552fdd9a7c65ad0f01a51a0489ce740e636b805e
Benjamin Siret <benjaminsiret@outlook.fr>
2021-03-17 11:54:33 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: 'pages#home'
+  authenticated :user do
+    resources :themes, only: [:index, :show] do
+      resources :quizzes, only: :index do
+        resources :user_quizzes, only: [:new, :create]
+      end
+    end
+    resources :quizzes, only: :show
+    resources :questions, only: :show
+    resources :responses, only: :show
+    resources :user_quizzes, only: :show
+  end
+
+  # our statics pages
+  # get '/explainer', to: 'pages#explainer'
 end
