938b0530eafa8d5e1f07da0854c55b3be40cfae0
Benjamin Siret <benjaminsiret@outlook.fr>
2021-03-19 11:57:35 +0100

    changes

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,14 +1,15 @@
 class ThemesController < ApplicationController
   def index
-    # if params[:query].present?
-    #   sql_query = "name ILIKE :query OR description ILIKE :query"
-    #   @themes = Theme.where(sql_query, query: "%#{params[:query]}%")
-    # else
+    if params[:query].present?
+      sql_query = "name ILIKE :query OR description ILIKE :query"
+      @themes = Theme.where(sql_query, query: "%#{params[:query]}%")
+    else
     @themes = Theme.all
-    # end
+    end
   end
 
   def show
     @theme = Theme.find(params[:id])
+    @quizzes = @theme.quizzes
   end
 end
