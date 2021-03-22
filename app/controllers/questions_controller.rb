8f2cca9cdeb61b9af2294b26d5142838ec29fb82
Theo Agriodos <theo.agriodos@gmail.com>
2021-03-22 18:00:43 +0100

    reglage quiz

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,8 @@
 class QuestionsController < ApplicationController
   def show
-
+    @question = Question.find(params[:id])
+    # params[:response_id]
     "on veut sur afficher la question numéro"
-    @question = Question.find(question.order)
     "on veut afficher les réponses affiliées à la question"
     "on veut pouvoir cocher une réponse"
     "on veut valider notre réponse"
@@ -11,14 +11,19 @@ class QuestionsController < ApplicationController
     "une fois que l'user à tout lu il clique sur question suivante"
     "et rebelote pour la quuestion numéro deux jusqu'à plus de question et donc affichage de la page félicitation"
 
-    @question = Question.find(params[:id])
-    @responses = @question.responses
+    # @question = Question.find(params[:id])
+    # @responses = @question.responses
 
-    @response = Response.find(params[:question_id])
-    if @response.result == false
-      "cadre explication rose"
-    else
-      "cadre explication vert"
-    end
+    # @response = Response.find(params[:question_id])
+    # if @response.result == false
+    #   "cadre explication rose"
+    # else
+    #   "cadre explication vert"
+    # end
+
+    @display_answer = !params.dig(:answer, :response_id).blank?
+    @next_question = Question.find_by(quiz: @question.quiz, order: @question.order + 1)
+
+    #est ce que la réponse est vraie ? (pour utiliser avec les couleurs)
   end
 end
