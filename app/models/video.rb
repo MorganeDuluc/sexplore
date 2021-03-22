7eb864f44ffcb45d5590fc6dcb85912e460bc2df
morgane duluc <morgane.duluc@gmail.com>
2021-03-22 11:24:35 +0100

    search bar repare

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,11 @@ class Video < ApplicationRecord
   belongs_to :theme
 
   validates :title, presence:true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title,
+    against: [ :title ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
