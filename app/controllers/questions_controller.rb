055086264575298ca5d0a40a14d10f2f8fbeb3d5
Benjamin Siret <benjaminsiret@outlook.fr>
2021-03-23 16:34:46 +0100

    explication view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,16 +5,20 @@ class QuestionsController < ApplicationController
     @display_answer = @response_id.present?
     @correct_answer = Response.find(@response_id).result if @response_id.present?
     @next_question = Question.find_by(quiz: @question.quiz, order: @question.order + 1)
-    raise
+    if @display_answer
+      redirect_to explication_question_path(question: @question, correct_answer: @correct_answer, next_question: @next_question)
+    else
+      render :show
+    end
 
-  end
+end
 
   def explication
-    @question = Question.find(params[:id])
-    @response_id = params.dig(:answer, :response_id)
-    @display_answer = @response_id.present?
-    @correct_answer = Response.find(@response_id).result if @response_id.present?
-    @next_question = Question.find_by(quiz: @question.quiz, order: @question.order + 1)
-
+    @question = Question.find(params[:question])
+    @correct_answer = params[:correct_answer]== "true"
+    if !params[:next_question].nil?
+      @next_question = Question.find(params[:next_question])
+    end
   end
+
 end
